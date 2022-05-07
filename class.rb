c59c57400704a9ea6f6defcc122f3a721528a5da
Chi Zhang <95065844+warlocksa@users.noreply.github.com>
2022-05-06 21:17:27 -0600

    Add files via upload

---
+++ class.rb
@@ -0,0 +1,17 @@
+class player
+  #player switch
+class question
+  #put the question
+class answer
+  #put the answer and confirm it
+class game{ player: { score: }, turn: {question: if (question.answer)puts count } }
+  #count the point and start a new turn
+  #when score == 0, game over 
+x % 2 = 1 player1
+x % 2 = 0 player2
+
+def Loop (turn)
+ turn ++ unless score == 0
+end
+puts game over 
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require ‘player.rb’
+require ‘game.rb’
+game = Game.new
+game.play_game
