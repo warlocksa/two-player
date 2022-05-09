2c407ba11e1003d5b53e79fff086603ecfc348a1
Chi Zhang <95065844+warlocksa@users.noreply.github.com>
2022-05-08 20:58:32 -0600

    Update player.rb

--- player.rb
+++ player.rb
@@ -10,12 +10,12 @@ class Game
     num1 = rand(1 .. 20)
     num2 = rand(1 .. 20)
     answer = num1 + num2
-    puts “Player #{current_player}: what does #{num1} plus #{num2} equal?”
+    puts "Player #{current_player}: what does #{num1} plus #{num2} equal?"
     input = gets.chomp.to_i
     if answer == input
-      puts “Player #{current_player}: YES! You are correct”
+      puts "Player #{current_player}: YES! You are correct"
     else
-      puts “Player #{current_player}: Seriously? No!”
+      puts "Player #{current_player}: Seriously? No!"
       if current_player == 1
         player_first.modify_life
       else
@@ -24,7 +24,7 @@ class Game
     end
   end
   def print_res
-     “P1: #{player_first.life}/3 vs P2: #{player_second.life}/3"
+     "P1: #{player_first.life}/3 vs P2: #{player_second.life}/3"
   end
   def change_current_player
     if current_player == 1
@@ -35,10 +35,10 @@ class Game
   end
   def check_winner
     if player_first.life == 0
-      puts “Player #{2} wins with a score of #{player_second.life}/3”
+      puts "Player #{2} wins with a score of #{player_second.life}/3"
       return true
     elsif player_second.life == 0
-      puts “Player #{1} wins with a score of #{player_first}/3"
+      puts "Player #{1} wins with a score of #{player_first}/3"
       return true
     end
     return false
@@ -47,14 +47,13 @@ class Game
     while true
       prompt_question
       puts print_res
-      puts “----- NEW TURN -----”
+      puts "----- NEW TURN -----"
       change_current_player
       if check_winner
-        puts “----- GAME OVER -----”
-        puts “Good bye!”
+        puts "----- GAME OVER -----"
+        puts "Good bye!"
         break
       end
     end
   end
 end
-9:10
